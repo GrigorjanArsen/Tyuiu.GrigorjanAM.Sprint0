924dd8f021ddd22a18c6b35cf72fc71b31f5da1b
djura <djura@DESKTOP-2ATL71Q>
2023-09-10 16:30:02 +0500

    Add project files.

---
+++ Tyuiu.GrigorjanAM.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.GrigorjanAM.Sprint0.Task0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
