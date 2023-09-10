852d35dcbde93c9d62881675a864ca7a3699ab76
Tyuiu.GrigorjanAM@outlook.com <djuravlik323@gmail.com>
2023-09-10 16:52:30 +0500

    Task1

---
+++ Tyuiu.GrigorjanAM.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.GrigorjanAM.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Арсен";
+            int age = 18;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + ", мне " + age + " лет.");
+            Console.ReadKey();
+        }
+    }
+}
